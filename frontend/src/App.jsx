bc812cbde342e1e4c997e7dda2ee599a47792fcd
Ashad2-Git <purchaseinfo9198@gmail.com>
2026-03-09 18:11:05 -0400

    update1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import { useState } from 'react'
+import ExpenseForm from './components/ExpenseForm'
 
 function App() {
 
+    const [expenses, setExpenses] = useState([]);
+
+    const addExpense = (expense) => {
+      setExpenses([...expenses, expense])
+    };
+
+    const deleteExpense = (id) => {
+      setExpenses(expenses.filter((item) => item.id !== id))
+    }
+
   return (
     <>
-      <div className='text-5xl text-amber-300 font-bold'>App running</div>
+      <h1>Expense Tracker</h1>
+      <ExpenseForm  addExpense={addExpense}/>
     </>
   )
 }
---
+++ frontend/src/components/ExpenseForm.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+
+export default function ExpenseForm({ addexpense }) {
+
+    const [name, setName] = useState("");
+    const [amount, setAmount] = useState("");
+    const [category, setCategory] = useState("Food");
+
+    const handleSubmit = (e) => {
+        e.preventDefalut();
+
+        
+    }
+
+    return (
+        <>
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        placeholder="Expense Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                        className="border border-black"></input>
+                    <input 
+                        placeholder="Amount"
+                        value={amount}
+                        type="Number"
+                        onChange={(e) => setAmount(e.target.value)}
+                        required
+                        className="border border-black"></input>
+                    <select
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    >
+                        <option>Food</option>
+                        <option>Travel</option>
+                        <option>Marketing</option>
+                        <option>Utilities</option>
+                        <option>Others</option>
+                    </select>
+
+                    <button>Add Expense</button>
+
+                </form>
+            </section>
+        </>
+    )
+}
